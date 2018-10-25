e8f79d5e9264797f357a171da1395d0335e2e12d
Krzysztof Barański <krzysztof.baranski@aptiv.com>
2018-10-25 15:45:02 +0200

    Wersja testowa

--- background.js
+++ background.js
@@ -1,22 +1,65 @@
-// import { urls } from './consts';
+function changeIcon (data) {
+    if (!data) {
+        console.log('[bacground doStaff] No data');
+        return;
+    }
+    var tabId = data.tabId;
+    var iconName = data.icon;
+    console.log('[changeIcon]', tabId, iconName);
 
-// Called when the user clicks on the browser action
-// console.log('!! ', Consts, chrome);
+    if (tabId) {
+        chrome.browserAction.setIcon({
+            path: iconName,
+            tabId: tabId
+        });
 
-function doStaffWithDom (domContent) {
-    console.warn('!! dommm', domContent);
+        setTimeout(function () {
+            chrome.browserAction.setIcon({
+                path: 'green.png',
+                tabId: tabId
+            });
+        }, 2000);
+
+        alert('Paused?');
+    }
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log('!! tab', tab);
-    // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow:true},
-        function(tabs) {
-            console.log('!! tabs', tabs);
-         var activeTab = tabs[0];
-         chrome.tabs.sendMessage(activeTab.id, 
-             {"message": "clicked_browser_action"}, 
-             doStaffWithDom
-         );
-   });
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     console.log('!! tab', tab);
+//     // Send a message to the active tab
+//     chrome.tabs.query({ active: true, currentWindow: true },
+//         function (tabs) {
+//             console.log('!! tabs', tabs);
+//             var activeTab = tabs[0];
+//             chrome.tabs.sendMessage(activeTab.id,
+//                 { "message": "tab-updated", tabId: activeTab.id },
+//                 doStaffWithDom
+//             );
+//         });
+// });
+
+function checkURL (url) {
+    if (!url) {
+        return;
+    }
+
+    return /.*:\/\/.*\.youtube\.com\/.*/.test(url);
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // console.log('!! tab', tabId, changeInfo, tab);
+    if (changeInfo.status !== 'complete' || !checkURL(tab.url)) {
+        console.log('return', checkURL(tab.url));
+        return;
+    }
+    console.log('run extension');
+    chrome.browserAction.setIcon({
+        path: 'green.png',
+        tabId: tabId
+    });
+    
+    chrome.tabs.sendMessage(tabId,
+        { message: 'tab-updated', tabId: tabId },
+        changeIcon
+    );
 });
--- content.js
+++ content.js
@@ -1,15 +1,31 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-      console.log('aa r', request);
-      if( request.message === "clicked_browser_action") {
-        unpause(sendResponse);
-      }
-   }
+    function (request, sender, sendResponse) {
+        console.log('aa r', request, sender, sendResponse);
+        if (request.message === "tab-updated") {
+            // set up the mutation observer
+            var observer = new MutationObserver(function (mutations, me) {
+                // `mutations` is an array of mutations that occurred
+                // `me` is the MutationObserver instance
+                var divMain;
+                divMain = document.querySelectorAll('div#main.style-scope.yt-confirm-dialog-renderer');
+                console.log('@@@ Observer ', divMain);
+                if (divMain.length) {
+                    btn = divMain[0].querySelectorAll('paper-button');
+                    if (btn.length) {
+                        btn[0].click();
+                        sendResponse({ tabId: tabId, icon: 'paused.png' });
+                        // me.disconnect(); // stop observing
+                        return;
+                    }
+                }
+            });
+
+            // start observing
+            observer.observe(document, {
+                childList: true,
+                subtree: true
+            });
+        }
+        return true;
+    }
 );
-
-function unpause(/* sendResponse */){
-    var el = document.querySelectorAll('yt-button-renderer #button.yt-button-renderer');
-
-    console.log('Toggle', el);
-    // sendResponse();
-}
