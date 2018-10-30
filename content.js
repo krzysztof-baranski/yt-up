e7cb06b5536500ab67d9de64588da2dfd6ffa50c
Krzysztof Barański <krzysztof.baranski@aptiv.com>
2018-10-30 15:33:13 +0100

    Return kiedy area hidden

--- content.js
+++ content.js
@@ -6,16 +6,19 @@ chrome.runtime.onMessage.addListener(
             var observer = new MutationObserver(function (mutations, me) {
                 // `mutations` is an array of mutations that occurred
                 // `me` is the MutationObserver instance
-                console.log('@@@ observer works!');
+                // console.log('@@@ observer works!');
                 var divMain;
                 divMain = document.querySelectorAll('paper-dialog');
-                console.log('@@@ paper-dialog', divMain);
+                // console.log('@@@ paper-dialog', divMain);
                 if (divMain.length) {
+                    if (divMain[0].getAttribute('aria-hidden') === 'true') {
+                        return true;
+                    }
                     btn = divMain[0].getElementsByTagName('paper-button');
                     if (btn.length) {
                         console.log('@@@ Observer ', btn, btn[0].click);
                         
-                        // btn[0].click();
+                        btn[0].click();
                         sendResponse({ tabId: request.tabId, icon: 'paused.png' });
                         // me.disconnect(); // stop observing
                         return;
